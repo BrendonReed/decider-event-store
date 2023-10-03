f2a948c6c45bf96e971a6e48f8eb4fb0be368cef
Brendon Reed <brendon.reed@slalom.com>
2023-10-02 23:34:25 -0600

    toward background task for materializer

--- app/src/main/java/decider/event/store/App.java
+++ app/src/main/java/decider/event/store/App.java
@@ -5,6 +5,7 @@ import java.time.OffsetDateTime;
 import java.util.ArrayList;
 import java.util.UUID;
 import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Schedulers;
 
 public class App {
 
@@ -57,13 +58,14 @@ public class App {
                     return storage.saveEvents(newEvents, streamId);
                 });
         main.blockLast(Duration.ofMinutes(1));
-        // var listener = storage.registerListener("foo_channel").map(x -> {
-        //     System.out.println(x);
-        //     System.out.println(x.getName());
-        //     System.out.println(x.getParameter());
-        //     return x;
-        // });
-        // listener.blockLast(Duration.ofMinutes(5));
+        // listen on background task, not sure this works right
+        var listener = storage.registerListener("foo_channel").map(x -> {
+            System.out.println(x);
+            System.out.println(x.getName());
+            System.out.println(x.getParameter());
+            return x;
+        });
+        listener.subscribeOn(Schedulers.parallel()).blockLast(Duration.ofMinutes(5));
 
         System.out.println("final events: " + events);
         System.out.println("calc final state:" + Utils.fold(new Decider.State(0), events, Decider::evolve));
